003bf6bb59b7f062a1fb12b0c0bad92b61e55742
Joël Cox <joel@joelcox.nl>
2012-11-22 16:19:39 +0100

    Also added some syntactic sugar to the utils spec, using the new  method.

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,17 +5,12 @@ describe('A utility function called', function() {
 	describe('`distance`', function() {
 
 		it('calculate the distance between nodes', function() {
-			n1 = new Graaf.Node('foo');
-			n1.x = 2;
-			n1.y = 3;
-
-			n2 = new Graaf.Node('bar');
-			n2.x = 3
-			n2.y = 5
+			n1 = new Graaf.Node('foo').at(2, 3);
+			n2 = new Graaf.Node('bar').at(3, 5);
 
 			expect(Graaf.utils.distance(n1, n2)).toEqual(2.23606797749979);
 		});
 
 	});
-	
+
 });
